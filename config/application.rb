9d81aa66f2a7e127422069f2c4441c2892f5610b
Dan <dchen12812@gmail.com>
2016-03-07 11:26:16 -0800

    fixed typo with mongoid xml

--- config/application.rb
+++ config/application.rb
@@ -20,9 +20,11 @@ module Places
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    #Load mongoid db
+    Mongoid.load!('./config/mongoid.yml')
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
 
-  Mongoid.load('./config/application.rb')
 end
