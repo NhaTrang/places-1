c308d7906420f133d2fce0f1e1b3cfaadb66d09f
Dan <dchen12812@gmail.com>
2016-03-26 23:13:47 -0700

    changed routing

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root 'places#index'
+  resources :places, only: [:index, :show]
+  get 'photos/:id/show', to: 'photos#show', as 'photos_show'
 end
