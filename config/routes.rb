8d1f801dec3d919429c7cbd22774a09d39c2ce46
Dan <dchen12812@gmail.com>
2016-03-28 00:46:11 -0700

    updated routes and controler

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'photos/show'
-
   root 'places#index'
   resources :places, only: [:index, :show]
-  #get 'photos/:id/show', to: 'photos#show', as 'photos_show'
+  get 'photos/:id/show', to: 'photos#show', as: 'photos_show'
 end
