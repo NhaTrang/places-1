497eaf862ffd725ee1c4ef118c6cd116287c6c7e
Dan <dchen12812@gmail.com>
2016-03-28 00:34:04 -0700

    init photos controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'photos/show'
+
   root 'places#index'
   resources :places, only: [:index, :show]
   #get 'photos/:id/show', to: 'photos#show', as 'photos_show'
